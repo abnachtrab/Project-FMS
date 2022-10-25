5309e207e0b33ac754cdcf1b484996ff4d8c2d15
abnachtrab <abnachtrab@gmail.com>
2022-10-25 00:01:09 -0700

    Added fullscreen functionality via a button in the top right of the screen. Properly resizes canvas and all elements within (I think, fingers crossed).

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 //// Constants
 
 // Width and height of canvas (in pixels) Default: 854x480
-const W = 854
-const H = 480
+let W = 854
+let H = 480
 
 // Multiplier for transition speed. A transMult of 1 lasts 1 second
 const transMult = 1
@@ -31,6 +31,7 @@ let optionsButton
 let mainMenuButton
 
 // Misc
+let fs
 let title
 let volumeSliderLabel
 let volumeSliderCounter
@@ -42,10 +43,33 @@ let levelCount = 8
 let levelButtons = []
 
 function setup() {
+    // Set window size
+    W = windowWidth
+    H = windowWidth * 9 / 16
     // Create canvas
     createCanvas(W, H)
     // Set framerate
     frameRate(240)
+    // Fullscreen button
+    fs = createButton("&#x26F6;")
+    fs.position(W - 35, -5)
+    fs.mousePressed(() => {
+        fullscreen(!fullscreen())
+    })
+    fs.mouseOver(() => {
+        fs.style("color", "#aaa")
+    })
+    fs.mouseOut(() => {
+        fs.style("color", "white")
+    })
+    fs.style("background-color", "rgba(0, 0, 0, 0)")
+    fs.style("color", "white")
+    fs.style("font-family", "sans-serif")
+    fs.style("border", "none")
+    fs.style("text-align", "center")
+    fs.style("text-decoration", "none")
+    fs.style("display", "inline-block")
+    fs.style("font-size", "32px")
     // Resize transition div
     document.getElementById("transition").style.width = (W + "px")
     document.getElementById("transition").style.height = (H + "px")
@@ -54,40 +78,40 @@ function setup() {
     backgroundDesign = loadImage("images/design-bg.png")
     // Create Title
     title = createElement("h1", "Learn2Draw")
-    title.size(W, H/4)
-    title.position(0, -W/16)
+    title.size(W, H / 4)
+    title.position(0, -W / 16)
     title.style("text-align", "center")
-    title.style("font-size", W/8 + "px")
+    title.style("font-size", W / 8 + "px")
     title.style("color", "#FF8C00")
     // Create start button
     startButton = createButton("Start")
-    startButton.size(W/4, H/8)
-    startButton.position(W/2-W/8, H/2-H/8)
+    startButton.size(W / 4, H / 8)
+    startButton.position(W / 2 - W / 8, H / 2 - H / 8)
     startButton.mousePressed(() => {
         transitioning = true
         gameState = "level select_"
     })
-    startButton.style("font-size", W/16 + "px")
+    startButton.style("font-size", W / 16 + "px")
     startButton.style("font-family", "TheFountainofWishes")
     // Create options button
     optionsButton = createButton("Options")
-    optionsButton.size(W/4, H/8)
-    optionsButton.position(W/2-W/8, H/2+H/16)
+    optionsButton.size(W / 4, H / 8)
+    optionsButton.position(W / 2 - W / 8, H / 2 + H / 16)
     optionsButton.mousePressed(() => {
         transitioning = true
         gameState = "options_"
     })
-    optionsButton.style("font-size", W/16 + "px")
+    optionsButton.style("font-size", W / 16 + "px")
     optionsButton.style("font-family", "TheFountainofWishes")
     // Create main menu button
     mainMenuButton = createButton("< Main Menu")
-    mainMenuButton.size(W/6.5, H/16)
+    mainMenuButton.size(W / 6.5, H / 16)
     mainMenuButton.position(-2, -2)
     mainMenuButton.mousePressed(() => {
         transitioning = true
         gameState = "main menu_"
     })
-    mainMenuButton.style("font-size", W/28 + "px")
+    mainMenuButton.style("font-size", W / 28 + "px")
     mainMenuButton.style("font-family", "TheFountainofWishes")
     mainMenuButton.style("color", "orange")
     mainMenuButton.style("font-weight", "bold")
@@ -105,39 +129,39 @@ function setup() {
     })
     // Create volume slider
     volumeSlider = createSlider(0, 100, 50, 1)
-    volumeSlider.size(W/5, H/16)
-    volumeSlider.position(W/2-W/8, H/3)
+    volumeSlider.size(W / 5, H / 16)
+    volumeSlider.position(W / 2 - W / 8, H / 3)
     volumeSlider.style("background-color", "rgba(0, 0, 0, 0.4)")
     // Create volume slider label
     volumeSliderLabel = createElement("h2", "Volume")
-    volumeSliderLabel.size(W/4, H/16)
-    volumeSliderLabel.position(W/2-W/8, H/3-H/8)
-    volumeSliderLabel.style("font-size", W/24 + "px")
+    volumeSliderLabel.size(W / 4, H / 16)
+    volumeSliderLabel.position(W / 2 - W / 8, H / 3 - H / 8)
+    volumeSliderLabel.style("font-size", W / 24 + "px")
     volumeSliderLabel.style("font-family", "TheFountainofWishes")
     volumeSliderLabel.style("color", "orange")
     volumeSliderLabel.style("text-align", "center")
     // Create volume slider counter
     volumeSliderCounter = createElement("h2", vol + "%")
-    volumeSliderCounter.size(W/5, H/16)
-    volumeSliderCounter.position(W/2+W/12, H/3-H/24)
-    volumeSliderCounter.style("font-size", W/32 + "px")
+    volumeSliderCounter.size(W / 5, H / 16)
+    volumeSliderCounter.position(W / 2 + W / 12, H / 3 - H / 24)
+    volumeSliderCounter.style("font-size", W / 32 + "px")
     volumeSliderCounter.style("font-family", "TheFountainofWishes")
     volumeSliderCounter.style("color", "orange")
     // Create level buttons
     for (let i = 0; i < levelCount; i++) {
-        levelButtons[i] = createButton("Level " + (i+1))
-        levelButtons[i].size(W/4, H/8)
+        levelButtons[i] = createButton("Level " + (i + 1))
+        levelButtons[i].size(W / 4, H / 8)
         levelButtons[i].position(
-            W/2-(35*W/128) + (i%2)*(W/4+W/32),
-            H/2-H/5 + Math.floor(i/2)*H/6
+            W / 2 - (35 * W / 128) + (i % 2) * (W / 4 + W / 32),
+            H / 2 - H / 5 + Math.floor(i / 2) * H / 6
         )
         levelButtons[i].mousePressed(() => {
             transitioning = true
-            gameState = "level " + (i+1) + "_"
+            gameState = "level " + (i + 1) + "_"
         })
-        levelButtons[i].style("font-size", W/16 + "px")
+        levelButtons[i].style("font-size", W / 16 + "px")
         levelButtons[i].style("font-family", "TheFountainofWishes")
-        if(i > 0) {
+        if (i > 0) {
             levelButtons[i].addClass("locked")
         }
     }
@@ -146,8 +170,55 @@ function setup() {
 }
 
 function draw() {
+    function resizeStuff() {
+        resizeCanvas(W, H)
+        document.getElementById("transition").style.width = (W + "px")
+        document.getElementById("transition").style.height = (H + "px")
+        title.size(W, H / 4)
+        title.style("font-size", W / 8 + "px")
+        startButton.size(W / 4, H / 8)
+        startButton.position(W / 2 - W / 8, H / 2 - H / 8)
+        startButton.style("font-size", W / 16 + "px")
+        optionsButton.size(W / 4, H / 8)
+        optionsButton.position(W / 2 - W / 8, H / 2 + H / 16)
+        optionsButton.style("font-size", W / 16 + "px")
+        mainMenuButton.size(W / 6.5, H / 16)
+        mainMenuButton.position(-2, -2)
+        mainMenuButton.style("font-size", W / 28 + "px")
+        volumeSlider.size(W / 5, H / 16)
+        volumeSlider.position(W / 2 - W / 8, H / 3)
+        volumeSliderLabel.size(W / 4, H / 16)
+        volumeSliderLabel.position(W / 2 - W / 8, H / 3 - H / 8)
+        volumeSliderLabel.style("font-size", W / 24 + "px")
+        volumeSliderCounter.size(W / 5, H / 16)
+        volumeSliderCounter.position(W / 2 + W / 12, H / 3 - H / 24)
+        volumeSliderCounter.style("font-size", W / 32 + "px")
+        for (let i = 0; i < levelCount; i++) {
+            levelButtons[i].size(W / 4, H / 8)
+            levelButtons[i].position(
+                W / 2 - (35 * W / 128) + (i % 2) * (W / 4 + W / 32),
+                H / 2 - H / 5 + Math.floor(i / 2) * H / 6
+            )
+            levelButtons[i].style("font-size", W / 16 + "px")
+        }
+        fs.position(W - 35, -5)
+    }
+
+    // Fullscreen resize
+    if (fullscreen() && (W !== displayWidth)) {
+        W = displayWidth
+        H = displayWidth * 9 / 16
+        resizeStuff()
+    } else if (!fullscreen() && (W !== windowWidth)) {
+        W = windowWidth
+        H = windowWidth * 9 / 16
+        resizeStuff()
+        // Resize transition div
+        document.getElementById("transition").style.width = (W + "px")
+        document.getElementById("transition").style.height = (H + "px")
+    }
     // Main menu
-    if(gameState === "main menu") {
+    if (gameState === "main menu") {
         // Hide things from other states
         mainMenuButton.hide()
         volumeSlider.hide()
@@ -163,7 +234,7 @@ function draw() {
         optionsButton.show()
     }
     // Level select
-    else if(gameState === "level select") {
+    else if (gameState === "level select") {
         // Hide things from other states
         startButton.hide()
         optionsButton.hide()
@@ -171,8 +242,8 @@ function draw() {
         background(backgroundDesign)
         title.html("Level Select")
         mainMenuButton.show()
-        for(let i=0;i<levelCount;i++) {
-            if(levelButtons[i].hasClass("locked")) {
+        for (let i = 0; i < levelCount; i++) {
+            if (levelButtons[i].hasClass("locked")) {
                 levelButtons[i].style("background-image", "url(images/lock.png)")
                 levelButtons[i].style("background-size", "50% 100%")
                 levelButtons[i].style("background-repeat", "no-repeat")
@@ -191,7 +262,7 @@ function draw() {
         }
     }
     // Options menu
-    else if(gameState === "options") {
+    else if (gameState === "options") {
         // Hide things from other states
         startButton.hide()
         optionsButton.hide()
@@ -205,44 +276,44 @@ function draw() {
         // Update volume slider counter
         vol = volumeSlider.value()
         volumeSliderCounter.html(vol + "%")
-    } else if(/[0-9]+$/.test(gameState)){
+    } else if (/[0-9]+$/.test(gameState)) {
         // Hide everything
         startButton.hide()
         optionsButton.hide()
-        for(let i=0;i<levelCount;i++) {
+        for (let i = 0; i < levelCount; i++) {
             levelButtons[i].hide()
         }
         // Draw level
         background(backgroundDesign)
         mainMenuButton.show()
-        if(gameState === "level 1") {
+        if (gameState === "level 1") {
             level1()
-        } else if(gameState === "level 2") {
+        } else if (gameState === "level 2") {
             level2()
-        } else if(gameState === "level 3") {
+        } else if (gameState === "level 3") {
             level3()
-        } else if(gameState === "level 4") {
+        } else if (gameState === "level 4") {
             level4()
-        } else if(gameState === "level 5") {
+        } else if (gameState === "level 5") {
             level5()
-        } else if(gameState === "level 6") {
+        } else if (gameState === "level 6") {
             level6()
-        } else if(gameState === "level 7") {
+        } else if (gameState === "level 7") {
             level7()
-        } else if(gameState === "level 8") {
+        } else if (gameState === "level 8") {
             level8()
         }
     }
     // Fancy transition bs cause I was bored one night
-    if(transitioning) {
+    if (transitioning) {
         // If transition is complete, stop transitioning
-        if(transTime<0) {
+        if (transTime < 0) {
             transitioning = false
             transTime = 0
             transDir = 1
         }
         // If screen is fully black, change game state and invert transition direction
-        else if(transTime>1.0) {
+        else if (transTime > 1.0) {
             transDir = -1
             transTime = 1
             gameState = gameState.slice(0, -1)
@@ -253,15 +324,16 @@ function draw() {
             transTime += (deltaTime / (500 * transMult)) * transDir
         }
     }
-    if(bones)background(bonez)
+    if (bones) background(bonez)
+
     function level1() {
         background(backgroundGradient)
         fill(0)
-        if(mouseIsPressed) {
+        if (mouseIsPressed) {
             drawnPos.push([mouseX, mouseY])
         }
         drawnPos.forEach((i) => {
-            circle(i[0],i[1],20)
+            circle(i[0], i[1], 20)
         })
 
     }
@@ -269,15 +341,15 @@ function draw() {
 
 
 function keyPressed() {
-    if(keyCode === 27) {
+    if (keyCode === 27) {
         transitioning = true
         gameState = "main menu_"
     } else {
-        ee+=key
-        if(ee.includes("bones") && !bones && !ee.includes("no  bones") && !ee.includes("nobones")) {
+        ee += key
+        if (ee.includes("bones") && !bones && !ee.includes("no  bones") && !ee.includes("nobones")) {
             ee = ""
             bones = true
-        } else if((ee.includes("nobones") || ee.includes("no bones")) && bones) {
+        } else if ((ee.includes("nobones") || ee.includes("no bones")) && bones) {
             ee = ""
             bones = false
         }
